21cea62ddf279c92180acd20b576dfc452f74065
Sylvieho <hws5127a@ensfsi.onmicrosoft.com>
2025-03-20 19:01:41 +0100

    7-AfficherMarche

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -1,5 +1,7 @@
 package frontiere;
 
+import java.util.Iterator;
+
 import controleur.ControlAfficherMarche;
 
 public class BoundaryAfficherMarche {
@@ -10,6 +12,14 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if(infosMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			System.out.println(nomAcheteur + ", vous trouverez au marché:");
+			for (int i = 0; i<infosMarche.length; i = i+3) {
+				System.out.println("- " + infosMarche[i] + " qui vend " + infosMarche[i + 1] + " " +  infosMarche[i + 2]);
+			}
+		}
 	}
 }
